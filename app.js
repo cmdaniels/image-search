3bc83f1df712830ae4936e275e5b54b1b0fa4823
Cody Daniels <codydaniels@gmail.com>
2015-12-31 18:43:16 -0700

    Connect Flickr API to search endpoint

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 var path = require('path');
+var Flickr = require('flickrapi');
+require('dotenv').load();
+
+// Flickr API settings
+var flickrOptions = {
+  api_key: process.env.FLICKR_API_KEY,
+  secret: process.env.FLICKR_API_KEY
+};
 
 // Initialize express app
 var app = express();
@@ -16,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Serve favicon
-app.use(favicon(__dirname + '/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 // Route index
 app.get('/', function(req, res) {
@@ -25,13 +33,45 @@ app.get('/', function(req, res) {
 
 // Route recentQueries
 app.get('/recentQueries', function(req, res) {
-  res.json({});
+  res.send(database);
 });
 
 // Route image search
 app.get('/:search', function(req, res) {
   var offset = req.query.offset || 0;
-  res.json({});
+  Flickr.tokenOnly(flickrOptions, function(error, flickr) {
+    if (error) {
+      console.error(error);
+      process.end(-1);
+    }
+    flickr.photos.search({
+      text: req.params.search,
+      page: offset,
+      per_page: 10,
+      safe_search: 2
+    }, function(err, result) {
+      if (error) {
+        console.error(error);
+        process.end(-1);
+      }
+      var photoObj;
+      var resArray = [];
+      result.photos.photo.forEach(function(photo) {
+        photoObj = {
+          url: 'https://www.flickr.com/photos/' + photo.owner + '/' + photo.id + '/sizes/l/',
+          snippet: photo.title,
+          thumbnail: 'https://www.flickr.com/photos/' + photo.owner + '/' + photo.id + '/sizes/t/',
+          context: 'https://www.flickr.com/photos/' + photo.owner + '/' + photo.id
+        };
+        resArray.push(photoObj);
+      });
+      database.push({
+        term: req.params.search,
+        when: new Date()
+      });
+      res.send(resArray);
+    });
+  });
 });
 
 // Start express server
